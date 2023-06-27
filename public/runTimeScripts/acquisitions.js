82d6264dc58c56bd9caa6e9a916d289b4f84d5e1
juanpardo123 <129297821+juanpardo123@users.noreply.github.com>
2023-06-27 00:11:47 -0400

    final

---
+++ public/runTimeScripts/acquisitions.js
@@ -0,0 +1,12 @@
+{/* <div class="card">
+            <h1>Comments</h1>
+            <!-- Form to add new comments -->
+            <form id="commentForm">
+              <textarea id="commentInput" placeholder="Enter your comment"></textarea>
+              <button type="submit">Submit</button>
+            </form>
+            <div id="commentContainer"></div>
+          
+            <script src="/runTimeScripts/blog.js"></script>
+
+        </div> */}
